7c22cca225ac36111d455e7b9f7de0e08df118c0
Aditya Gawali <gawaliaditya1483@gmail.com>
2026-09-19 17:05:41 +0530

    fix: update whatsapp-web.js and force stable webVersionCache for pairing code issue

--- whatsapp-microservice/index.js
+++ whatsapp-microservice/index.js
@@ -11,6 +11,10 @@ const client = new Client({
     authStrategy: new LocalAuth(), // Saves the login session so you don't have to scan QR every time
     puppeteer: {
         args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'] // Required for running on platforms like Render
+    },
+    webVersionCache: {
+        type: "remote",
+        remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html"
     }
 });
 
